d5dd2c9790de5806bd117e674a4cb454ab590afa
emihled <emihledumo@gmail.com>
2025-10-29 15:55:08 +0200

    basic script

---
+++ oparetions-types.js
@@ -0,0 +1,13 @@
+const number1 = 20;
+const number2 = 40;
+str1 = number1.toString();
+sum = number1 + number2;
+product= number1 * number2;
+console.log(number1);
+console.log(number2);
+console.log(str1)
+console.log(sum);
+console.log(product);
+
+console.log("EMIHLE")  
+
